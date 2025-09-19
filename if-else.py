2ce5ed19fe4648b10d02dbcaf44e06494e4545ab
wochuna <wochunayvonne@gmail.com>
2025-09-19 09:56:23 +0300

    add if- else

---
+++ if-else.py
@@ -0,0 +1,14 @@
+grade = int(input("Enter your grade: "))
+
+if grade >= 85:
+    print("A")
+elif grade >= 75:
+    print("B")
+elif grade >= 65:
+    print("C")
+elif grade >= 50:
+    print("D")
+elif grade >= 0:
+    print("E")
+else:
+    print("Invalid Input")
